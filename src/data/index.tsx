0adcab35e2a6abcad3a3889700c79c8411aae4c7
Andrey Carvalho <andreycarvalho@icloud.com>
2022-04-02 17:46:07 -0300

    dummy data

---
+++ src/data/index.tsx
@@ -0,0 +1,21 @@
+import airdropsData from "./airdrops.json"
+import coursesData from "./courses.json"
+import gamesData from "./games.json"
+import calendarData from "./calendar.json"
+import highlightsData from "./highlights.json"
+import newsData from "./news.json"
+import whitelistsData from "./whitelists.json"
+import partnersData from "./partners.json"
+import coursesHighlightsData from "./coursesHighlights.json"
+
+export {
+  airdropsData,
+  coursesData,
+  gamesData,
+  calendarData,
+  highlightsData,
+  newsData,
+  whitelistsData,
+  partnersData,
+  coursesHighlightsData
+}
